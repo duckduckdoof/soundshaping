ae9aabbe78d545b3d0fc64d8acb4e84971b08eb4
cwscott3 <cwscott3@ncsu.edu>
2026-06-27 17:38:35 -0400

    Added updates to notes module

--- modules/notes.py
+++ modules/notes.py
@@ -6,9 +6,20 @@ Author: Caleb Scott
 ---
 
 Descriptions of notes, and their MIDI numbers.
+
+This note system assumes that C3 is MIDI 60.
 """
 
+# IMPORTS
+import math
+import re
+
+from typing import Any
+
 # CONSTANTS
+
+# Regex for Note notation
+NOTE_RE = "(?P<n>[A-G]#?)(?P<oct>-?[0-9])"
 
 # MIDI/freq tuning: 440 Hz.
 BASE_FREQ = 440
@@ -16,6 +27,8 @@ BASE_MIDI = 69
 MIDI_MAX = 128
 
 # 12 note scale lookup
+# NOTE: it may be helpful to create a more flexible system for
+#   flats/sharps with respect to their musical scales.
 NOTE_LOOKUP = {
     0:  "C",
     1:  "C#",
@@ -31,37 +44,24 @@ NOTE_LOOKUP = {
     11: "B"
 }
 
+NOTE_LOOKUP_INV = {
+    "C": 0,
+    "C#": 1,
+    "D": 2,
+    "D#": 3,
+    "E": 4,
+    "F": 5,
+    "F#": 6,
+    "G": 7,
+    "G#": 8,
+    "A": 9,
+    "A#": 10,
+    "B": 11
+}
+
 # Octave arrangement
 BASE_OCTAVE = -2
 STD_OCTAVE = 3
-
-# FUNCTIONS
-
-# MIDI conversions
-def midi_to_freq(
-    midi_no: int,
-    base_freq: int = BASE_FREQ,
-    base_midi: int = BASE_MIDI
-) -> float:
-    """
-    Given a midi number, calculate its frequency.
-    """
-    if midi_no < 0 or midi_no > MIDI_MAX:
-        return -1.0
-    return base_freq * (2 ** ((midi_no - base_midi)/12))
-
-def midi_to_note(midi_no: int) -> tuple:
-    """
-    Given a midi number, determine the corresponding musical note and its octave.
-
-    This references a table which is used in Logic Studio.
-    """
-    if midi_no < 0 or midi_no > MIDI_MAX:
-        raise Exception(f"MIDI number outside max MIDI range [0 - {MIDI_MAX}")
-    n_len = len(NOTE_LOOKUP)
-    note = midi_no % n_len
-    octave = int(((midi_no - note) / n_len) + BASE_OCTAVE)
-    return f"{NOTE_LOOKUP[note]}", octave
 
 # CLASSES
 class Note:
@@ -71,5 +71,162 @@ class Note:
 
     def __init__(
         self,
+        repr: Any,
+        r_type: str
     ):
-        pass
+        # Based on the r_type, determine how to build the note.
+        # This can either be 'midi' (int) or 'note' (str)
+        if r_type == 'midi':
+            self.m = int(repr)
+            self.n = midi_to_note(self.m)
+            self.f = midi_to_freq(self.m)
+        elif r_type == 'note':
+            self.n = str(repr)
+            self.m = note_to_midi(self.n)
+            self.f = note_to_freq(self.n)
+        else:
+            raise Exception("Invalid representation type for a Note.")
+        res = re.search(NOTE_RE, self.n)
+        if not res:
+            raise Exception("Octave not properly calculated in Note creation.")
+        self.oct = int(res.group("oct"))
+
+class StdNotes:
+    """
+    This class generates valid notes on the full MIDI range.
+    """
+
+    def __init__(self) -> None:
+        self.notes = []
+
+        # Raw range of all midi notes
+        for i in range(MIDI_MAX):
+            self.notes.append(Note(i, 'midi'))
+
+        # Organized by octave
+        self.oct_notes = {}
+        for note in self.notes:
+            if note.oct not in self.oct_notes:
+                self.oct_notes[note.oct] = [note]
+            else:
+                self.oct_notes[note.oct].append(note)
+
+        # Organized by note string (which includes name+oct)
+        self.descr_notes = {n.n: n for n in self.notes}
+
+        # Organized by midi number
+        self.midi_notes = {n.m: n for n in self.notes}
+
+    def get_by_notes(self, notes: list, octaves: list) -> list:
+        """
+        Returns note objects based on their string notation.
+
+        TODO: mark for sharp/flat notations
+        """
+        results = []
+        for octave in octaves:
+            results.extend([n for n in self.oct_notes[octave] if n.n in notes])
+        return results
+
+    def get_scale(self, tonic: Note, key: list) -> list:
+        """
+        Given a selected octave, base note (called 'tonic'), and key (maj/min/etc.),
+        return all possible notes which describe this scale.
+
+        'key' is a list of relative offsets (e.g. - +2, +2, +1, +2, +2, +2, +1 = maj)
+
+        NOTE we are taking advantage of the fact that our notes are organized by midi
+        number: each index is equivalent to taking +/- a half step, or one midi number.
+        """
+        results = [tonic]
+        idx = tonic.m
+        for offset in key:
+            idx += offset
+            results.append(self.notes[idx])
+        return results
+
+    def semitone_len(self, n1: Note, n2: Note) -> int:
+        """
+        Returns positive distance between two notes.
+        """
+        return abs(n1.semitone_dist(n2))
+
+    def print_notes(self) -> None:
+        current_oct = self.notes[0].oct
+        for n in self.notes:
+            if n.oct != current_oct:
+                current_oct = n.oct
+                print()
+            print(str(n), end=' ')
+        print()
+
+# FUNCTIONS
+def midi_to_freq(
+    midi_no: int,
+    base_freq: int = BASE_FREQ,
+    base_midi: int = BASE_MIDI,
+    midi_max: int = MIDI_MAX
+) -> float:
+    """
+    Given a midi number, calculate its frequency.
+    """
+    if midi_no < 0 or midi_no > midi_max:
+        return -1.0
+    return base_freq * (2 ** ((midi_no - base_midi)/12))
+
+def midi_to_note(
+    midi_no: int,
+    midi_max: int = MIDI_MAX,
+    base_oct: int = BASE_OCTAVE
+) -> str:
+    """
+    Given a midi number, determine the corresponding musical note and its octave.
+
+    This references a table which is used in Logic Studio.
+    """
+    if midi_no < 0 or midi_no > midi_max:
+        raise Exception(f"MIDI number outside max MIDI range [0 - {midi_max}]")
+    n_len = len(NOTE_LOOKUP)
+    note = midi_no % n_len
+    octave = int(((midi_no - note) / n_len) + base_oct)
+    return f"{NOTE_LOOKUP[note]}{octave}"
+
+def note_to_midi(note: str) -> int:
+    """
+    Given a note description (ex: 'A5'), determine its midi number.
+    """
+    res = re.search(NOTE_RE, note)
+    if not res:
+        raise Exception("Invalid Note notation.")
+    n = NOTE_LOOKUP_INV[res.group("n")]
+    base = (int(res.group("oct")) - BASE_OCTAVE) * len(NOTE_LOOKUP_INV)
+    return n + base
+
+def note_to_freq(note: str) -> float:
+    """
+    Given a note description (ex: 'A5'), determine its frequency.
+    """
+    midi = note_to_midi(note)
+    return midi_to_freq(midi)
+
+# These methods require high precision in the frequency to get
+# back to its corresponding note/midi number. Useful, but not the most helpful.
+def freq_to_midi(
+    freq: float,
+    base_midi: int = BASE_MIDI,
+    base_freq: int = BASE_FREQ
+) -> int:
+    """
+    Given a midi number, calculate it's frequency.
+    """
+    return int(base_midi + 12 * math.log2(freq/base_freq))
+
+def freq_to_note(freq: float) -> str:
+    midi = freq_to_midi(freq)
+    return midi_to_note(midi)
+
+def semitone_dist(n1: Note, n2: Note) -> int:
+    """
+    Returns signed distance current note is from another.
+    """
+    return n2.m - n1.m
