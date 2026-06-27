4c7075a0a22f62835a9d8780291313ad6ef32493
duckduckdoof <duckduckdoof@gmail.com>
2026-06-27 18:14:53 -0400

    Added waves and updated sound functions

---
+++ modules/sound.py
@@ -0,0 +1,135 @@
+"""
+sound.py
+
+Author: Caleb Scott
+
+---
+
+Module for sound information and playing sounds.
+"""
+
+# IMPORTS
+from modules.waves import Wave
+import numpy as np
+import sounddevice as sd
+
+# CONSTANTS
+SAMPLE_RATE = 44100
+NUM_CHANNELS = 2
+
+sd.default.samplerate = SAMPLE_RATE
+sd.default.channels = NUM_CHANNELS
+
+# CLASSES
+class ADSREnvelope:
+    """
+    Class for modifying the amplitude/level of some sound using a classic ADSR envelope.
+
+    Designed to be paired with the Sound class, to modify generated sounds.
+
+    Durations for each phase is expressed as a fraction in [0,1] space.
+    The total durations for each phase must add to 1.
+    """
+
+    def __init__(
+        self,
+        atk_dur: float,
+        atk_height: float,
+        dec_dur: float,
+        dec_height: float,
+        sus_dur: float,
+        rel_dur: float
+    ) -> None:
+        self.atk_dur = atk_dur
+        self.atk_height = atk_height
+        self.dec_dur = dec_dur
+        self.dec_height = dec_height
+        self.sus_dur = sus_dur
+        self.rel_dur = rel_dur
+
+class Harmonics:
+    """
+    Class for specifying the harmonics of a sound, given a base frequency.
+    """
+
+    def __init__(self, harmonics: list, levels: list) -> None:
+        self.freqs = harmonics
+        self.base = harmonics[0]
+        self.levels = levels
+
+class Sound:
+    """
+    Sound data to be played.
+    You can create this information directly, or use Note information.
+
+    This sound can be parameterized by any wave function, so
+    long as it returns an np.ndarray
+    """
+
+    def __init__(
+        self,
+        freq: int,
+        duration: float,
+        amplitude: float,
+        wave: Wave,
+        adsr: ADSREnvelope | None = None,
+        sample_rate: int = SAMPLE_RATE
+    ) -> None:
+        self.freq = freq
+        self.duration = duration
+        self.amplitude = amplitude
+        self.wave = wave
+        self.sample_rate = sample_rate
+
+        # Might help to clamp the duration to a minimum, since sample rate is finite.
+        self.n_samples = int(self.sample_rate * self.duration)
+        self.adsr = adsr
+
+    def gen_sound_data(self) -> np.ndarray:
+        """
+        Instead of playing the sound directly, generate the sound
+        data to then be played on something.
+
+        Helpful if you want to generate and join sound data,
+        and then play it.
+        """
+        t_points = np.linspace(0, self.duration, self.n_samples, False)
+
+        # Amplitude is determined by 'amplitude' parameter (global scaling)
+        # and the provided adsr envelope
+        waveform = self.amplitude * np.multiply(
+                self.gen_adsr(),
+                self.wave.f(2 * np.pi * self.freq * t_points)
+        )
+        return waveform
+
+    def gen_adsr(self) -> np.ndarray:
+        """
+        If an adsr is provided, generate amplitude data.
+        """
+        base = np.ones(self.n_samples)
+        if not self.adsr:
+            return base
+
+        # Attack
+        atk_len = int(self.adsr.atk_dur * self.n_samples)
+        base[0: atk_len] = np.linspace(0.0, self.adsr.atk_height, atk_len, False)
+
+        # Decay
+        dec_len = int(self.adsr.dec_dur * self.n_samples)
+        dec_offset = atk_len + dec_len
+        base[atk_len: dec_offset] = np.linspace(self.adsr.atk_height, self.adsr.dec_height, dec_len, False)
+
+        # Sustain
+        sus_len = int(self.adsr.sus_dur * self.n_samples)
+        sus_offset = dec_offset + sus_len
+        base[dec_offset: sus_offset] = self.adsr.dec_height * np.ones(sus_len)
+
+        # Release
+        rel_len = int(self.adsr.rel_dur * self.n_samples)
+        base[sus_offset:] = np.linspace(self.adsr.dec_height, 0.0, rel_len, False)
+
+        return base
+# MAIN
+if __name__ == 'main':
+    pass
---
+++ modules/waves.py
@@ -0,0 +1,59 @@
+"""
+waves.py
+
+Author: Caleb Scott
+
+---
+
+Module for describing different wave types.
+"""
+
+# IMPORTS
+import numpy as np
+
+# CLASSES
+class Wave:
+
+    def __init__(self, w_type: str):
+        if w_type is 'sin':
+            self.f = sin
+        elif w_type is 'f_square':
+            self.f = f_square
+        elif w_type is 'f_triangle':
+            self.f = f_triangle
+        elif w_type is 'f_saw':
+            self.f = f_saw
+        elif w_type is 'square':
+            self.f = nf_square
+        else:
+            raise Exception("Wave type not recognized")
+
+# FUNCTIONS
+
+# Waveforms
+def sin(x):
+    return np.sin(x)
+
+# Waveforms (other than sine)
+# These are fourier approximations of the follwing wave shapes:
+
+def f_square(x, n: int = 100):
+    c = 4/np.pi
+    return c * sum([np.sin(x * (2*k - 1))/(2*k - 1) for k in range(1, n+1)])
+
+def f_triangle(x, n: int = 100):
+    c = 8/(np.pi**2)
+    return c * sum([((-1)**k) * np.sin(x * (2*k - 1))/((2*k - 1)**2) for k in range(1, n+1)])
+
+def f_saw(x, n: int = 100):
+    c = 2/np.pi
+    return c * sum([((-1)**k) * np.sin(x * k)/k for k in range(1, n+1)])
+
+# Non-fourier descriptions
+
+def nf_square(x):
+    return np.sign(sin(x))
+
+# MAIN
+if __name__ == 'main':
+    pass
